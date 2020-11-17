c6b3c57498b7ab3a53e2af5b0a05f81aa8b39e0b
Noah Shin <noahshin11@gmail.com>
2020-11-17 17:58:38 +0900

    [트렌디 신재훈] Add: user models.py, Core app

--- user/models.py
+++ user/models.py
@@ -1,3 +1,42 @@
-from django.db import models
+from django.db       import models
 
-# Create your models here.
+from core            import models as core_models
+
+class User(core_models.TimeStampedModel):
+    nick_name       = models.CharField(max_length=100)
+    password        = models.CharField(max_length=45)
+    email           = models.EmailField(max_length=100)
+    user_name       = models.CharField(max_length=100, null=True)
+    phone_number    = models.CharField(max_length=20, null=True)
+
+    class Meta:
+        db_table    = 'users'
+
+    def __str__(self):
+        return self.nick_name
+
+class Seller(core_models.TimeStampedModel):
+    name        = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    hash_tag    = models.TextField(null=True)
+    image_url   = models.URLField(max_length=1000)
+
+    class Meta:
+        db_table    = 'sellers'
+
+    def __str__(self):
+        return self.name
+
+class Destination(core_models.TimeStampedModel):
+    recipient       = models.CharField(max_length=100)
+    phone_number    = models.CharField(max_length=45)
+    address         = models.CharField(max_length=500)
+    memo            = models.TextField(null=True)
+    user            = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    default_flag    = models.BooleanField(default=True)
+
+    class Meta:
+        db_table    = 'destinations'
+
+    def __str__(self):
+        return self.recipient
