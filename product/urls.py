e116c0310b7a2ad71399eeb9c19656a7dae20e9a
dev-swd <tladnjsen@gmail.com>
2020-11-26 20:52:19 +0900

    [트랜디 심원두] Modified ProductListView for pull request. 1. ProductListView 안에 재훈의 SearchView 코드 merge. 2. ProductListView filtering, ordering 기능 수정.

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from product.views import ProductListView, ProductDetailView, SearchView
+from product.views import ProductListView, ProductDetailView
 
 urlpatterns = [
     path('', ProductListView.as_view()),
     path('/<int:product_id>', ProductDetailView.as_view()),
-    path('/search', SearchView.as_view()),
 ]
